f07c6c6f7d4de317bb13c176ab8904e2ce60b203
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 17:32:51 +0300

    Файл конфигурации установки виджета

---
+++ config/.install.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Файл конфигурации установки виджета.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'use'         => FRONTEND,
+    'id'          => 'gm.wd.kcaptcha',
+    'category'    => 'captcha',
+    'name'        => 'KCaptcha',
+    'description' => 'Protection against automatic spam',
+    'namespace'   => 'Gm\Widget\KCaptcha',
+    'path'        => '/gm/gm.wd.kcaptcha',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'GM MS'],
+        ['app', 'code' => 'GM CMS'],
+        ['app', 'code' => 'GM CRM'],
+    ]
+];
