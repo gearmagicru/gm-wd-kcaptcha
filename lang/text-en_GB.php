b3e87ec427e205f2dfecf3847d16e9803af192fc
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 17:34:41 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'KCaptcha',
+    '{description}' => 'Protection against automatic spam',
+
+   // Settings
+   '{settings.title}' => 'Widget settings "KCaptcha"',
+   // Settings: поля
+   'JPEG quality' => 'JPEG quality',
+   'JPEG quality of CAPTCHA image (bigger is better quality, but larger file size)' => 'JPEG quality of CAPTCHA image (bigger is better quality, but larger file size)',
+   'Foreground color' => 'Foreground color',
+   'Background color' => 'Background color',
+   'Type image' => 'Type image',
+   'Credits line' => 'Credits line',
+   'Set to false to remove credits line. Credits adds 12 pixels to image height' => 'Set to false to remove credits line. Credits adds 12 pixels to image height',
+   'No spaces' => 'No spaces',
+   'Increase safety by prevention of spaces between symbols' => 'Increase safety by prevention of spaces between symbols',
+   'Fonts dir' => 'Fonts dir',
+   'String length' => 'String length',
+   'Alphabet' => 'Alphabet',
+   'Allowed symbols' => 'Allowed symbols',
+   'Symbols used to draw CAPTCHA' => 'Symbols used to draw CAPTCHA',
+   'Fluctuation amplitude' => 'Fluctuation amplitude',
+   'Symbols vertical fluctuation amplitude' => 'Symbols vertical fluctuation amplitude',
+   'No white noise' => 'No white noise',
+   'No black noise' => 'No black noise',
+   'Sizes' => 'Sizes',
+   'Width' => 'Width, px',
+   'Height' => 'Height, px',
+    // Settings: всплывающие сообщения / текст
+   'Widget settings successfully changed' => 'Widget settings "KCaptcha" successfully changed.',
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'KCaptcha',
+    '{description}' => 'Защита от автоматического спама',
+
+   // Settings
+   '{settings.title}' => 'Настройка виджета "KCaptcha"',
+   // Settings: поля
+   'JPEG quality' => 'Качество JPEG',
+   'JPEG quality of CAPTCHA image (bigger is better quality, but larger file size)' => 'Качество JPEG изображения CAPTCHA (более высокое качество, но больший размер файла)',
+   'Foreground color' => 'Цвет переднего плана',
+   'Background color' => 'Цвет фона',
+   'Type image' => 'Вид изображения',
+   'Credits line' => 'Надпись (название сайта)',
+   'Set to false to remove credits line. Credits adds 12 pixels to image height' => 'Установите значение false, чтобы удалить надпись. Надпись добавляют 12 пикселей к высоте изображения',
+   'No spaces' => 'Без пробелов',
+   'Increase safety by prevention of spaces between symbols' => 'Повышение безопасности за счет предотвращения пробелов между символами',
+   'Fonts dir' => 'Путь к шрифтам',
+   'String length' => 'Длина строки',
+   'Alphabet' => 'Алфавит',
+   'Allowed symbols' => 'Разрешенные символы',
+   'Symbols used to draw CAPTCHA' => 'Символы, используемые для рисования капчи',
+   'Fluctuation amplitude' => 'Амплитуда колебаний',
+   'Symbols vertical fluctuation amplitude' => 'Амплитуда вертикальных колебаний символа',
+   'No white noise' => 'Без белого шума',
+   'No black noise' => 'Без черного шума',
+   'Sizes' => 'Размеры',
+   'Width' => 'Ширина, пкс',
+   'Height' => 'Высота, пкс',
+    // Settings: всплывающие сообщения / текст
+   'Widget settings successfully changed' => 'Настройки виджета "KCaptcha" успешно сохранены.',
+];
